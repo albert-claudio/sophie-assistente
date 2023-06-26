8721d5da5869604e249a1cea68b8a8647e0003b5
Albert Claudio <albertclaudiolink@gmail.com>
2023-06-26 16:34:54 -0300

    Sophie Beta

---
+++ www/js/comandos.js
@@ -0,0 +1,26 @@
+comandos = [
+    {
+     entrada:'acessar memórias',
+     retorno_escrito: 'Sim, senhor!',
+     retorno_falado: 'Sim, senhor!',
+     rota: '/memorias/',
+     especial: false   
+    },
+    {
+        entrada:'qual é o seu nome',
+        retorno_escrito: 'Meu nome é Sophie!',
+        retorno_falado: 'Meu nome é Sophie',
+        rota: false,
+        especial:false 
+    },
+    {
+        entrada:'enviar e-mail',
+        retorno_escrito: 'Para quem gostaria de enviar?',
+        retorno_falado: 'Para quem gostaria de enviar?',
+        rota: false,
+        especial: function(){
+            falar('Para quem gostaria de enviar?','Para quem gostaria de enviar?',false);
+            app.dialog.alert('Disparou o evento!');
+        } 
+    },
+]
